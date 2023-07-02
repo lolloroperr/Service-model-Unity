3f8a8e221df20845a128f5a3525a0ba9b7f3fa56
lolloroperr <138375038+lolloroperr@users.noreply.github.com>
2023-07-02 14:37:01 +0200

    Add files via upload
    Input part

---
+++ Services/Randomizer/RandomService.cs
@@ -0,0 +1,12 @@
+﻿using Random = UnityEngine.Random;
+
+namespace CodeBase.Services.Randomizer
+{
+  public class RandomService : IRandomService
+  {
+    public int Next(int min, int max)
+    {
+      return Random.Range(min, max);
+    }
+  }
+}
